8b30d177fb8910112b1e1a56a045ca282d3a94bd
ilusion linux <ilusion.linux@gmail.com>
2012-09-17 23:06:29 +0000

    Version Inicial

--- fuentes/crypto.cpp
+++ fuentes/crypto.cpp
@@ -1,36 +1,129 @@
+/*
+ * crypto
+ * Programa encriptador
+ * 
+ * autor ilusion-linux
+ * */
 #include <iostream>
+#include <cstring>
+#include <cstdlib>
 
 using std::cout;
 using std::endl;
 
-int estado;
+int intOperacion; //Variable para almacenar tipo de operacion
+	//	0  encriptar
+	//	1  desencriptar
+				  
+int intDir;//Variable para identificar si es directorio o documento
+	//	0  directorios
+	//	1  archivos
+		   
+char * chrPassword;//Variable que contiene la contraseña ingresada
+char * chrRuta;//Variable que contiene la ruta directorio
+char * chrArchivo;//Variable que contiene la ruta del archivo
 
-void determinarParametros(char *);
+void mostarInformacion();
+/*
+ * Funcion para mostrar caracteristicas del programa, asi como las 
+ * diferentes opciones que este permite.
+ * */
 
 int main(int argC, char * argV[])
 {
-	estado=0;
+	intOperacion=-1;
 	
-	for(int x=0; x<argC; x++)
+	int intEstado=0;//Variable para analizar si se esta leendo un
+		//parametro o un valor de parametro.
+	
+	char ** chrAuxiliar;//Variable tipo puntero a puntero, para
+		//referenciar el valor donde se almacenara algun parametro.
+						
+	
+	for(int x=1; x<argC; x++)
 	{
-		cout<<argV[x]<<endl;
+		switch(intEstado)
+		{
+			case 0:
+				if(strcasecmp(argV[x], "l")==0)
+				{
+					intEstado=1;
+					chrAuxiliar=&chrRuta;
+				}
+				else if(strcasecmp(argV[x], "a")==0)
+				{
+					intEstado=1;
+					chrAuxiliar=&chrArchivo;
+				}
+				else if(strcasecmp(argV[x], "p")==0)
+				{
+					intEstado=1;
+				}
+				else if(strcasecmp(argV[x], "e")==0)
+				{
+					intOperacion=0;
+				}
+				else if(strcasecmp(argV[x], "d")==0)
+				{
+					intOperacion=1;
+				}
+				else if(strcasecmp(argV[x], "i")==0)
+				{
+					x=argC;
+					intEstado=-1;
+					intOperacion=-1;
+					mostarInformacion();
+				}
+				else
+				{
+					cout<<"Error:"<<endl;
+					cout<<"El parametro "<<argV[x]<<" no es un "
+					    <<"parametro vaido."<<endl<<endl;
+					cout<<"Consulte crypto i, para obtener ayuda"<<endl;
+					x=argC;
+					intEstado=-1;
+					intOperacion=-1;
+				}
+			break;
+			case 1:
+				if(strcasecmp(argV[x], "l")==0 || strcasecmp(argV[x],
+					"a")==0 || strcasecmp(argV[x], "p")==0 ||
+					strcasecmp(argV[x], "e")==0 || strcasecmp(argV[x],
+					"d")==0 || strcasecmp(argV[x], "i")==0)
+				{
+					x=argC;
+					intEstado=-1;
+					intOperacion=-1;
+					cout<<"Error en la definicion de los parametros.";
+					cout<<endl;
+				}
+				else
+				{
+					chrRuta=argV[x];
+					intEstado=0;
+				}
+			break;
+		}
 	}
 	
-	switch(estado)
+	switch(intOperacion)
 	{
 		case 0:
+		
 		break;
 		case 1:
+		
+		break;
+		default:
+			cout<<"No ha indicado si desea encriptar o desencriptar.";
+			cout<<endl;
 		break;
 	}
 	
-	
-	cout<<endl<<endl;
-	cout<<"Hola mundo"<<endl;
 	return 0;
 }
 
-void determinarParametros(char * parametros)
+void mostarInformacion()
 {
-	cout<<"Funcion";
+	system("more crypto.info");
 }
