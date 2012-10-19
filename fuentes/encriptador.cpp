10e09bf6624aca8e19bc7449c0f175f049b7b934
ilusion linux <ilusion.linux@gmail.com>
2012-10-19 16:24:25 +0000

    Version Inicial

--- fuentes/encriptador.cpp
+++ fuentes/encriptador.cpp
@@ -10,7 +10,7 @@ using std::ios;
 using std::ofstream;
 using std::ifstream;
 
-const int Encriptador::intTamanio=((sizeof(char)*8)-1);                 //Definicion del tamanio de la variable char, segun cada plataforma y sistema operativo
+const int Encriptador::intTamanio=((sizeof(int)*8)-1);                  //Definicion del tamanio de la variable char, segun cada plataforma y sistema operativo
 const int Encriptador::intComparador=1<<intTamanio;
 
 Encriptador::Encriptador(char * pass, struct Buscador::directorio * dir,
@@ -73,12 +73,14 @@ void Encriptador::generarLlave()
 	agregarLlave(fabs(tempB));
 	agregarLlave(fabs(tempA)+fabs(tempB));
 	agregarLlave(fabs(fabs(tempA)-fabs(tempB)));
+	cout<<endl<<endl;
 }
 
 void Encriptador::agregarLlave(int elemento)
 {
 	llaves=new struct llave;
 	llaves->intLlave=elemento;
+	cout<<elemento<<"  |  ";
 	
 	if(temporalLlaves==NULL)
 	{
@@ -154,7 +156,6 @@ void Encriptador::encriptar()
 			}
 			
 			escritura<<((char)intEscritura);
-			intLlaveBinaria<<=1;
 			
 			if(intRecorrido==intTamanio)
 			{
@@ -162,8 +163,12 @@ void Encriptador::encriptar()
 				llaves=llaves->siguiente;
 				intLlaveBinaria=llaves->intLlave;
 			}
+			else
+			{
+				intLlaveBinaria<<=1;
+				++intRecorrido;
+			}
 			
-			++intRecorrido;
 		}
 		
 		lectura.close();
@@ -177,10 +182,98 @@ void Encriptador::encriptar()
 
 void Encriptador::desencriptar()
 {
+	int intRecorrido=0;
 	llaves=inicioLlaves;
+	int intLlaveBinaria=llaves->intLlave;
 	
 	while(elementos!=NULL)
 	{
+		char chrLectura;		
+		ifstream lectura(elementos->objeto.c_str(), ios::binary);
+		string strLectura="temp/bufferA.crypto";
+		ofstream escritura(strLectura.c_str(), ios::out);
+		
+		while(lectura>>chrLectura)
+		{
+			int intEscritura;
+			
+			if((intRecorrido%2)==0)
+			{
+				if((intLlaveBinaria && intComparador)==true)
+				{
+					if(((int)chrLectura)<(llaves->intLlave))
+					{
+						intEscritura=(llaves->intLlave)-((int)
+							chrLectura);
+						intEscritura=256-intEscritura;
+					}
+					else
+					{
+						intEscritura=((int)chrLectura)-(llaves->intLlave);
+						
+					}
+				}
+				else
+				{
+					if(((int)chrLectura)>(llaves->intLlave))
+					{
+						intEscritura=(llaves->intLlave)-((int)chrLectura);
+					}
+					else
+					{
+						intEscritura=((int)chrLectura)+(llaves->intLlave);
+					}
+				}
+			}
+			else
+			{
+				if((intLlaveBinaria && intComparador)==true)
+				{
+					if(((int)chrLectura)>(llaves->intLlave))
+					{
+						intEscritura=(llaves->intLlave)-((int)chrLectura);
+					}
+					else
+					{
+						intEscritura=((int)chrLectura)+(llaves->intLlave);
+					}
+				}
+				else
+				{
+					if(((int)chrLectura)<(llaves->intLlave))
+					{
+						intEscritura=(llaves->intLlave)-((int)
+							chrLectura);
+						intEscritura=256-intEscritura;
+					}
+					else
+					{
+						intEscritura=((int)chrLectura)-(llaves->intLlave);
+						
+					}
+				}
+			}
+			
+			escritura<<((char)intEscritura);
+			
+			if(intRecorrido==intTamanio)
+			{
+				intRecorrido=0;
+				llaves=llaves->siguiente;
+				intLlaveBinaria=llaves->intLlave;
+			}
+			else
+			{
+				intLlaveBinaria<<=1;
+				++intRecorrido;
+			}
+		}
+		
+		lectura.close();
+		escritura.close();
+		string copy=string("cp \"")+strLectura+string("\" \"")+
+			elementos->objeto+string("\"");
+		system(copy.c_str());
 		elementos=elementos->siguiente;
 	}
 }
