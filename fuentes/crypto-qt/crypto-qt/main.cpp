55f2e14b0d382059b5748eb0ebb314d1d967f0eb
ilusion linux <ilusion.linux@gmail.com>
2013-04-09 22:16:40 +0000

    crypto-0.05

--- fuentes/crypto-qt/crypto-qt/main.cpp
+++ fuentes/crypto-qt/crypto-qt/main.cpp
@@ -2,9 +2,30 @@
 #include <QRect>
 #include <QDesktopWidget>
 #include "vtnmenuprincipal.h"
+#include <iostream>
+#include <cstring>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
 
 int main(int argc, char *argv[])
 {
+    int expresion;
+
+    cout<<"Ingrese expresion: ";
+    //getline(cin, expresion);
+    cin>>expresion;
+
+    cout<<"------------------ "<<expresion<<"  "<<endl<<endl;
+
+    /*for(int x=0; x<expresion.length(); x++)
+    {
+        cout<<expresion[x]<<endl;
+    }*/
+
+
     QApplication::setStyle("Plastique");
 
     QApplication a(argc, argv);
